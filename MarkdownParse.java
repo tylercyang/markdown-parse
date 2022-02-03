3887c86681105c983a2ece0d96f46ddec6be398c
Tyler Yang <tcyang@ucsd.edu>
2022-02-02 20:33:02 -0800

    Fix the test case

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -59,18 +59,21 @@ public class MarkdownParse {
 
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
             
+            if (!markdown.substring(openParen + 1, closeParen).contains(".")) break;
+
             //run for loop and .contains on the substring?
             boolean check = false;
-            for ( String s : imageExtensions ) {
+            for (String s : imageExtensions) {
                 if (markdown.substring(openParen+1, closeParen).contains(s)){
                     check = true;
                     break;  
                 } 
             }
 
-            if (check == false && (nextCloseBracket != nextOpenBracket +1) && (nextCloseBracket == openParen -1)) {
+            if (check == false  && (nextCloseBracket == openParen -1)) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
+
             currentIndex = closeParen + 1;
         }
         return toReturn;
